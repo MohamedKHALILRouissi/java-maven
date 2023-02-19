05dbdf3a5107f1f72f25d89b387e258f344e391f
MohamedKHALILRouissi <124497891+MohamedKHALILRouissi@users.noreply.github.com>
2023-02-19 19:01:44 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -10,15 +10,15 @@ sh 'mvn package'
 
 def buildocker () {
 echo 'buidling docker ... ' 
-sh "docker build -t 192.168.1.18:${PORT_REPO}/${DOCKER_NAME}:${DOCKER_TAG} ."
+sh "docker build -t 192.168.1.18:${params.PORT_REPO}/${params.DOCKER_NAME}:${params.DOCKER_TAG} ."
 }
 
 
 def pushdocker () {
 echo "loging to nexus as ${NEXUS_CREDS_USR}"
-sh "echo ${NEXUS_CREDS_PSW} | docker login -u ${NEXUS_CREDS_USR} --password-stdin 192.168.1.18:${PORT_REPO}"
+sh "echo ${NEXUS_CREDS_PSW} | docker login -u ${NEXUS_CREDS_USR} --password-stdin 192.168.1.18:${params.PORT_REPO}"
 echo 'pushing docker image to nexus3 at 192.168.1.18 server'
-sh "docker push 192.168.1.18:${PORT_REPO}/${DOCKER_NAME}:${DOCKER_TAG}"
+sh "docker push 192.168.1.18:${params.PORT_REPO}/${params.DOCKER_NAME}:${params.DOCKER_TAG}"
 }
 
 return this 
