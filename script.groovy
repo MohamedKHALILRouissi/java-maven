dc328653cd604ed337931bfe09b35573a7575cd8
MohamedKHALILRouissi <124497891+MohamedKHALILRouissi@users.noreply.github.com>
2023-02-19 18:30:22 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -19,5 +19,6 @@ echo 'loging to nexus as ${USER}'
 sh 'echo ${NEXUS_CREDS_PSW} | docker login -u ${NEXUS_CREDS_USR} --password-stdin 192.168.1.18:${PORT}'
 echo 'pushing docker image to nexus3 at 192.168.1.18 server'
 sh 'docker push 192.168.1.18:${PORT}/${DOCKER_NAME}:{$DOCKER_TAG}'
+}
 
 return this 
